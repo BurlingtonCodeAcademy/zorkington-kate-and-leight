b3800b80d7c0f147fd52a237f80ad6fcb0146f40
Kate <kate.voorhes@gmail.com>
2018-09-27 17:19:55 -0400

    action word added sort of working but need to fix error messages

--- zorkington_draft.js
+++ zorkington_draft.js
@@ -21,23 +21,20 @@ class Room {
   // }
 }
 class Player {
-  constructor(status, location, inventory = []) {
+  constructor(status, location, inventory = {}) {
     this.status = status;
     this.location = location;
     this.inventory = inventory;
   }
   isInputValid(input) {
-    let inputArray = input.split(" ");
     let item = this.location.inventoryItem(input);
-    //console.log(item);
-    if (item && inputArray.includes(item.action)) {
-      return true;
-    } else if (item && inputArray.length > 1) {
-      console.log(`You can't ${inputArray.join(" this ")}.`, item.error);
-    } else {
-      console.log("Sorry, I did not understand.");
+    if (item){
+      let action = item.actionWord(input);
+      console.log(item, action);
+      return true;  
+    }else{
+      return false;
     }
-    return false;
   }
   outputMessage(input){
     if(!input.canStatusBeChanged()){
@@ -55,7 +52,11 @@ class Player {
   sorry(verb) {
     console.log(`Sorry, I don't know how to ${verb}.`);
   }
-}
+  
+
+};
+
+
 class Thing {
   constructor(name, status, action, message, error, otherThing = null) {
     this.name = name;
@@ -64,6 +65,14 @@ class Thing {
     this.message = message;
     this.error = error;
     this.dependsOn = otherThing;
+  }
+  actionWord(inputArray) {
+    console.log(this.action);
+    for (let i = 0; i < this.action.length; i++) {
+      if (inputArray.includes(this.action[i])) {
+        return this.action[i];
+      }
+    }
   }
   isLocked() {
     if (this.status.length === 1){
@@ -81,10 +90,14 @@ class Thing {
     }
     return false;
   }
-
   updateStatus() {
     this.status.shift();
     this.message.shift();
+  }
+  take(){
+    if (this.action.includes("take")){
+    user.inventory[this.name] = this;
+    }
   }
 }
 // NOTE: if Thing dependsOn something, then first message must be what outputs if dependsOn has not been unlocked yet.
@@ -92,7 +105,7 @@ class Thing {
 const sign = new Thing(
   "sign",
   ["unread", "read"],
-  "read",
+  ["read"],
   [
     "Welcome to Burlington Code Academy! Come on up to the second floor. If the door is locked, use the code 12345.",
     "You don't remember reading the sign? The code for the door is 12345!"
@@ -100,26 +113,40 @@ const sign = new Thing(
   "That would be selfish. How will other students find their way?"
 );
 const lock = new Thing(
-  "code", 
-["locked", "unlocked"], 
 "12345", 
-["The lock requires a code. Try reading the sign", "Success! The door unlocks!.", "It is already unlocked!"], 
+["locked", "unlocked"], 
+["code", "enter", "key"],
+["Bzzzzt! The door is still locked!", "Success! The door unlocks!.", "It is already unlocked!"], 
 "Wrong code. Try again.",
 sign);
 
 const door = new Thing(
   "door",
   ["closed", "opened"],
-  "open",
-  ["The door is locked.", "You opened the door!", "The door is already wide open!"],
+  ["open"],
+  ["The door is locked.", "Success! The door opens. You enter the foyer and the door shuts behind you.", "The door is already open!"],
   " ",
   lock
+);
+
+const paper = new Thing(
+  "paper",
+  ["not picked up", "picked up"],
+  ["take", "read", "pick"],
+  ["You pick up the paper and leaf through it looking for comics and ignoring the articles, just like everybody else does.", "You already took the paper"],
+  " ",
 );
 
 const MainSt182 = new Room(
   "182 Main St.\n You are standing on Main Street between Church and South Winooski. There is a door here. A keypad sits on the handle. On the door is a handwritten sign.",
   { north: null, south: null, east: null, west: null },
   [door, sign, lock]
+);
+
+const Foyer = new Room(
+  "You are in a foyer. Or maybe it's an antechamber. Or a vestibule. Or an entryway. Or an atrium. Or a narthex. But let's forget all that fancy flatlander vocabulary, and just call it a foyer. In Vermont, this is pronounced 'FO-ee-yurr'. A copy of Seven Days lies in a corner.",
+  { },
+  [paper]
 );
 const user = new Player("good", MainSt182);
 // { food: null, weapons: null, misc: null }
@@ -135,15 +162,26 @@ const startGame = async () => {
   console.log("You are here: " + user.location.description);
   console.log("\n");
   let input = await ask("> ");
+  let inputArray = input.split(" ");
+
   // option to quit
   if(input === "quit" || input === "end" || input === "exit"){
     process.exit();
   }
-  let item = user.location.inventoryItem(input);
-  if (user.isInputValid(input)) {
+  let item = user.location.inventoryItem(inputArray);
+  if (user.isInputValid(inputArray)) {
       user.outputMessage(item);
       console.log("\n");
+  }else{
+    console.log("error");
   }
+  // else outputError message
+  /*
+    else if (item || action && inputArray.length > 1) {
+      console.log(`You can't ${inputArray.join(" this ")}.`, item.error);
+    } else {
+      console.log("Sorry, I did not understand.");
+  }*/
   startGame();
 };
 
